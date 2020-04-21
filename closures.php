fb035070a9a6e3fc40c6b13155270d7c547b1ab1
Danna Lin <danna.lin@ctitv.com.tw>
2020-04-21 16:50:10 +0800

    modified

---
+++ closures.php
@@ -0,0 +1,16 @@
+<?php
+
+$printer = function($value) { echo "$value <br/>"; };
+
+$products = [ 'Tires' => 100,
+              'oil' => 10,
+              'Spark Plugs' => 4 ];
+
+$markup = 0.20;
+
+$apply = function(&$val) use ($markup) {
+            $val = $val * (1+$markup);
+         };
+array_walk($products, $apply);
+array_walk($products, $printer);
+?>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TLA Consulting Pty Ltd</title>
+        <link href="styles.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+
+        <!-- 網頁首頁 -->
+        <header>
+            <img src="logo.gif" alt="TLA logo" height="70" width="70" />
+            <h1>TLA Consulting</h1>
+        </header>
+        <!-- 選單 -->
+        <nav>
+            <div class="menuitem">
+                <a href="home.html">
+                    <img src="s-logo.gif" alt="" height="20" width="20" />
+                    <span class="menutext">Home</span>
+                </a>
+            </div>
+
+            <div class="menuitem">
+                <a href="contact.html">
+                    <img src="s-logo.gif" alt="" height="20" width="20" />
+                    <span class="menutext">Contact</span>
+                </a>
+            </div>
+
+            <div class="menuitem">
+                <a href="services.html">
+                    <img src="s-logo.gif" alt="" height="20" width="20" />
+                    <span class="menutext">Service</span>
+                </a>
+            </div>
+
+            <div class="menuitem">
+                <a href="about.html">
+                    <img src="s-logo.gif" alt="" height="20" width="20" />
+                    <span class="menutext">About</span>
+                </a>
+            </div>
+        </nav>
+    </body>
+</html>
